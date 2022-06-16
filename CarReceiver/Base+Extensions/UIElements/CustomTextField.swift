62c5defa33735e30158ffb24015c31840d908a82
Artem Mushtakov <a.vladimirovich@internet.ru>
2022-06-16 23:29:25 +0400

    Create login screen.

---
+++ CarReceiver/Base+Extensions/UIElements/CustomTextField.swift
@@ -0,0 +1,66 @@
+//
+//  CustomTextField.swift
+//  CarReceiver
+//
+//  Created by Artem Mushtakov on 16.06.2022.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+
+    // MARK: - Initial
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    // MARK: - Setup
+
+    private func setup() {
+        self.isSecureTextEntry = true
+        setupTextFieldView()
+    }
+
+    private func setupTextFieldView() {
+        
+        /// left view
+        self.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: self.frame.height))
+        self.leftViewMode = .always
+
+        /// right view
+        self.rightView = isSecureButton
+        self.rightViewMode = .always
+
+        /// setup layer
+        self.layer.backgroundColor = UIColor.white.cgColor
+        self.layer.cornerRadius = 10
+        self.layer.shadowRadius = 4.0
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOffset = CGSize(width: 0, height: 1.0)
+        self.layer.shadowOpacity = 0.2
+        self.layer.masksToBounds = false
+    }
+
+    // MARK: - UI elements
+
+    private lazy var isSecureButton = UIButton().then {
+        $0.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+        $0.setImage(UIImage(systemName: "lock.fill"), for: .normal)
+        $0.setImage(UIImage(systemName: "lock.open.fill"), for: .selected)
+        $0.addTarget(self, action: #selector(showHidePassword(_:)), for: .touchUpInside)
+    }
+
+    // MARK: - Actions
+
+    @objc private func showHidePassword(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        self.isSecureTextEntry = !sender.isSelected
+    }
+}
--- CarReceiver/Scenes/Login/LoginViewController.swift
+++ CarReceiver/Scenes/Login/LoginViewController.swift
@@ -26,7 +26,7 @@ class LoginViewController: UIViewController {
     private func setupView() {
         title = "Main"
         view.backgroundColor = .white
-        view.addSubview(contentView)
+        addSubviews()
         setupNavigationController()
         setupLayout()
     }
@@ -47,10 +47,76 @@ class LoginViewController: UIViewController {
         $0.contentMode = .scaleAspectFill
     }
 
+    private lazy var logoImageView = UIImageView().then {
+        let largeConfig = UIImage.SymbolConfiguration(pointSize: 100, weight: .bold, scale: .large)
+        let logoImage = UIImage(systemName: "car.2", withConfiguration: largeConfig)
+        $0.image = logoImage
+        $0.tintColor = R.color.orangeColor()?.withAlphaComponent(0.6)
+    }
+
+    private lazy var loginTextField = CustomTextField().then {
+        $0.placeholder = "Введите логин"
+        $0.font = R.font.nunitoRegular(size: 14)
+        $0.tintColor = .lightGray
+    }
+
+    private lazy var passwordTextField = CustomTextField().then {
+        $0.placeholder = "Введите пароль"
+        $0.font = R.font.nunitoRegular(size: 14)
+        $0.tintColor = .gray
+    }
+
+    private lazy var resetPassword = UIButton().then {
+        $0.setTitle("Забыли пароль?", for: .normal)
+        $0.titleLabel?.font = R.font.nunitoBold(size: 17)
+        $0.setTitleColor(.black, for: .normal)
+        $0.addTarget(self, action: #selector(openModuleResetPassword), for: .touchUpInside)
+    }
+
+    // MARK: - Setup Layout
+
+    private func addSubviews() {
+        view.addSubview(contentView)
+        view.addSubview(logoImageView)
+        view.addSubview(loginTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(resetPassword)
+    }
+
     private func setupLayout() {
 
         contentView.snp.makeConstraints {
             $0.center.height.width.equalToSuperview()
         }
+
+        logoImageView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalTo(UIScreen.main.bounds.height / 3)
+        }
+
+        loginTextField.snp.makeConstraints {
+            $0.centerY.equalTo(logoImageView.snp.bottom).offset(50)
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.height.equalTo(40)
+        }
+
+        passwordTextField.snp.makeConstraints {
+            $0.top.equalTo(loginTextField.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.height.equalTo(40)
+        }
+
+        resetPassword.snp.makeConstraints {
+            $0.top.equalTo(passwordTextField.snp.bottom).offset(20)
+            $0.leading.equalTo(20)
+            $0.trailing.equalTo(-20)
+            $0.height.equalTo(20)
+        }
+    }
+
+    @objc private func openModuleResetPassword() {
+        print("Open reset password")
     }
 }
